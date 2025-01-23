bc0e07a555a42aca1e6563f72bcd74ffed206163
Maaz Khan <60582716+evilewawa@users.noreply.github.com>
2025-01-23 10:59:20 -0500

    Update geminiParser.py

--- geminiParser.py
+++ geminiParser.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-genAiApiKey = 'AIzaSyBuwyMfUBrbUK-G6ZpBs3VLK-2QC90n5xM'
+genAiApiKey = # hidden
 genai.configure(api_key=genAiApiKey)
 model = genai.GenerativeModel("gemini-1.5-flash")
 # response = model.generate_content("What stock is this post talking about '$HIVE go boom to the moon'. answer only with the ticker symbol")
